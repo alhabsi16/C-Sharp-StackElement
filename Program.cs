b08958044f05a366a943960c28f4845c826a8a4e
alhabsi16 <mxmx8283@gmail.com>
2023-07-11 12:17:49 +0400

    Stack

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace C_Sharp_StackElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack Stack = new Stack(5);
+            Stack.Push(10);
+            Stack.Push(20);
+            Stack.Push(30);
+            Stack.Push(40);
+            Stack.Push(50);
+            Stack.Push(60);
+            Console.WriteLine(Stack.Pop());
+            Console.WriteLine(Stack.peek());
+            Console.WriteLine(Stack.Pop());
+            Stack.Push(60);
+            Console.WriteLine(Stack.peek());
+
+        }
+    }
+}
